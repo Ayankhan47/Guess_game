a984526aba0519d44e7a0eb41df060e5c5ccdcbd
AYAN KHAN <khanayan97553@gmail.com>
2024-02-29 15:39:24 +0530

    first commit

---
+++ src/guessgame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class guessgame {
+    public static void main(String[] args) {
+        int n,noofguess = 10;
+        Scanner sc = new Scanner(System.in);
+        int guess = (int)(Math.random()*100)+1;
+        do{
+            System.out.println("guess the number btw 1-100 : ");
+            n = sc.nextInt();
+            if(n==guess){
+                System.out.println("WINNER !!!!!!");
+                break;
+            }
+            if(--noofguess==0){
+                System.out.println("bade bahi aap haar gaye!!!");
+                break;
+            }if(guess>n){
+                System.out.println("think bigger !!!");
+            }else{
+                System.out.println("think smaller !!!");
+            }
+        }while(true);
+    }
+}
